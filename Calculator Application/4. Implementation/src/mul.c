3e2a4eb07bddbc0af4ddbf5098bb140edc8fec6c
99003524 <78556562+99003524@users.noreply.github.com>
2021-02-05 17:59:45 +0530

    Add files via upload

---
+++ Calculator Application/4. Implementation/src/mul.c
@@ -0,0 +1,38 @@
+#include <mul.h>
+
+int multiplication(int a,int b) {
+    //int a;
+    //int b;
+    int mul = 0;
+    printf(" the first number : %d", a);
+    //scanf("%d",&a);
+    printf(" the second number : %d",b);
+    //scanf("%d",&b);
+
+    if(a>0 && b>0) {
+        mul = a*b;
+        return mul;
+        //printf("The product is = %d", mul);
+    }
+
+    else if (a<0 && b<0) {
+        mul = a*b;
+        return mul;
+        //printf("The product is = %d", mul);
+    }
+    else if (a<0 && b>0) {
+        mul = a*b;
+        return mul;
+        //printf("The product is = %d", mul);
+    }
+    else  {
+        mul = a*b;
+        return mul;
+        //printf("The product is = %d", mul);
+    }
+    
+    
+    
+    return 0;
+
+}
