07b6658f4ce25b79678aa57dcd11fbd52ffc2b3b
99003518 <pranshi.lodha@ltts.com>
2021-02-05 23:26:40 +0530

    make

--- Calculator Application/4. Implementation/Test/Test_ncr.c
+++
@@ -1,9 +0,0 @@
-void test_ncr(void);
-CU_ncr_test(suite, "ncr", test_ncr);
-
-void test_ncr(void)
-{
-    CU_ASSERT(35 ==ncr(7,3));
-    CU_ASSERT(1 ==ncr(4,0));
-    CU_ASSERT(1 ==ncr(6,6));
-}
--- Calculator Application/4. Implementation/Test/Test_npr.c
+++
@@ -1,9 +0,0 @@
-void test_npr(void);
-CU_npr_test(suite, "npr", test_npr);
-
-void test_npr(void)
-{
-    CU_ASSERT(210 ==npr(7,3));
-    CU_ASSERT(1 ==npr(4,0));
-    CU_ASSERT(720 ==npr(6,6));
-}
