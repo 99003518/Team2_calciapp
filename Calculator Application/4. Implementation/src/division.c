d9bfde00e78a3c4c960b58c4a012cdb95433b55f
enskomali <78529367+enskomali@users.noreply.github.com>
2021-02-05 18:34:42 +0530

    Update division.c

--- Calculator Application/4. Implementation/src/division.c
+++ Calculator Application/4. Implementation/src/division.c
@@ -1,1 +1,20 @@
 
+#include <division.h>
+int division(n1,n2) {
+    int n1, n2; 
+    int quotient, remainder;
+    printf("Enter n1: ");
+    scanf("%d", &n1);
+    printf("Enter n2: ");
+    scanf("%d", &n2);
+
+    // Computes quotient
+    quotient = n1 / n2;
+
+    // Computes remainder
+    remainder = n1 % n2;
+
+    printf("Quotient = %d\n", quotient);
+    printf("Remainder = %d", remainder);
+    
+}
