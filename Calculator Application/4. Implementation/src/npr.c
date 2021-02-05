e8a22d99caafb6059e6f260c729430967b34f30b
stepin105110 <lodha.pranshi@gmail.com>
2021-02-05 18:22:42 +0530

    make

--- Calculator Application/4. Implementation/src/npr.c
+++ Calculator Application/4. Implementation/src/npr.c
@@ -1,4 +1,4 @@
-#include <calculator_operations.h>
+#include <npr.h>
 
 int add(int operand1, int operand2)
 {
--- Calculator Application/4. Implementation/src/percentage.c
+++ Calculator Application/4. Implementation/src/percentage.c
@@ -1,5 +1,6 @@
-#include<percentage.h>
-float percentage(float operand1,float operand2);
+#include <percentage.h>
+
+float percentage(float operand1,float operand2)
 {
     return (operand1/operand2)*100;
 }
