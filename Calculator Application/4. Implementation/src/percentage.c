a2184ae34ccdcf98e21f9a003d5db67a06c8a890
99003520 <78562135+99003520@users.noreply.github.com>
2021-02-06 15:49:52 +0530

    Update percentage.c

--- Calculator Application/4. Implementation/src/percentage.c
+++ Calculator Application/4. Implementation/src/percentage.c
@@ -1,18 +1,28 @@
 #include <percentage.h>
-
+/*include header file percentage.h in this program*/
 
 float percentage(float operand1,float operand2)
+    /*This is the function that is called from the main function */
 {
     float result;
-    /* Input 2 numbers */
+    /*Declare a variable result as float value*/
     printf("Enter 2 numbers: \n");
+    /*Input 2 numbers*/
     scanf("%f %f", &operand1,&operand2);
     /*Read the 2 numbers entered */
-    if(operand1==0 || operand2==0)
+    /*Use if-else statements*/
+    if(operand1==0)
     {
-        printf("Both the numbers entered should be greater than 0");
+        printf("The number entered should be greater than 0");
     }
-    /*The numbers entered should be greater than 0 else error will be displayed in calculator*/
+      /*The number entered should be greater than 0 else error will be displayed in calculator*/
+    /*When if condition fails then the compiler checks if else statement*/
+    else if(operand2==0)
+    {
+        printf("The number entered should be greater than 0");
+    }
+    /*The number entered should be greater than 0 else error will be displayed in calculator*/
+    /*When if-else condition fails then else statement will be executed*/
     else
     {
 
@@ -26,6 +36,7 @@ float percentage(float operand1,float operand2)
     /* The result is rounded off to 2 decimal places */
     }
     return result;
+    /*After code gets executed the result will be displayed*/
 }
 
     
