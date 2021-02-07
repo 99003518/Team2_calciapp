df81aae7f8509e96590200339261eebb14e1cd1f
99003522 <78556161+99003522@users.noreply.github.com>
2021-02-07 13:13:41 +0530

    Update ncr.c

--- Calculator Application/4. Implementation/src/ncr.c
+++ Calculator Application/4. Implementation/src/ncr.c
@@ -1,15 +1,15 @@
 #include <ncr.h>
 #include<stdio.h>
-int fact(int num)
+int fac(int num)
 {
     if (num <= 1) 
         return 1; 
-    return num*fact(num-1); 
+    return num*fac(num-1); 
 }
 int ncr(int n, int r)
 {
     int res;
-    res= fact(n)/(fact(n-r)*fact(r));
+    res= fac(n)/(fac(n-r)*fac(r));
     printf("\n The value of c(%d,%d) is:%d",n,r,res);
     return 0;
 }
