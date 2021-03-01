33b303b54d22fe8f0ee8e50f28d42312c3dce9f0
99003528 <78529367+enskomali@users.noreply.github.com>
2021-03-01 22:08:43 +0530

    Update modulo.c

--- Calculator Application/4. Implementation/src/modulo.c
+++ Calculator Application/4. Implementation/src/modulo.c
@@ -1,8 +1,8 @@
 #include<modulo.h>
-int modulo(int num1, int num2){
+int modulo(int num1, int num2){        //function declaration
 if((num1>=0)&&(num2>=1))
 {
-    return num1%num2;
+    return num1%num2;        //prints the modulo of two numbers
 }
 else{
     printf("error");
