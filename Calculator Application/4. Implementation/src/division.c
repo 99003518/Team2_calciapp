3b708a4e7343e7bab6e9317d3293a6d77d391c23
99003522 <78556161+99003522@users.noreply.github.com>
2021-03-01 17:10:09 +0530

    Update division.c

--- Calculator Application/4. Implementation/src/division.c
+++ Calculator Application/4. Implementation/src/division.c
@@ -7,35 +7,24 @@ int division(int n3,int n4) {
     scanf("%d", &n1);
     printf("Enter n2: ");
     scanf("%d", &n2);
-<<<<<<< HEAD
+
     if(n2==0)
     {
-        printf("Division can't be done");
+        printf("error");
     }
-    else
+    else if(n2>=1)
     {
         // Computes quotient
         quotient = n1 / n2;
     }
-    if(n2==0)
-    {
-        printf("Division can't be done");
-    }
-    else
-    {
-        // Computes remainder
-        remainder = n1 % n2;
-    }
-=======
-    
-    if((n1>=0)&&(n2>=1))
+    else if((n1>=0)&&(n2>=1))
     {
     // Computes quotient
     quotient = n1 / n2;
 
     // Computes remainder
     remainder = n1 % n2;
->>>>>>> 29a71d5663325e8c67d8722e1610fb4278b4a1eb
+
 
     printf("Quotient = %d\n", quotient);
     printf("Remainder = %d", remainder);
