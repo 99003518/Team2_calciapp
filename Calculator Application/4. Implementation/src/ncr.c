4e2df3df82fe762f29df7848b802f6d4f3ed39d2
99003519 <78556144+99003519@users.noreply.github.com>
2021-03-01 17:02:33 +0530

    Update

--- Calculator Application/4. Implementation/src/ncr.c
+++ Calculator Application/4. Implementation/src/ncr.c
@@ -9,7 +9,14 @@ int fac(int num)
 int ncr(int n, int r)
 {
     int res;
-    res= fac(n)/(fac(n-r)*fac(r));
-    printf("\n The value of c(%d,%d) is:%d",n,r,res);
+    if(n<0 || r<0)
+    {
+        printf("\n Can't be calculated for negative integers");
+    }
+    else
+    {
+        res= fac(n)/(fac(n-r)*fac(r));
+        printf("\n The value of c(%d,%d) is:%d",n,r,res);
+    }
     return 0;
 }
