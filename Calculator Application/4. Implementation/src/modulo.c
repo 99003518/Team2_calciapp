65717a9499fafb44dfdf959164cfc490b3fee8c3
99003524 <78556562+99003524@users.noreply.github.com>
2021-03-01 17:16:32 +0530

    Update modulo.c

--- Calculator Application/4. Implementation/src/modulo.c
+++ Calculator Application/4. Implementation/src/modulo.c
@@ -1,5 +1,13 @@
 #include<modulo.h>
-int modulo(int num1, int num2)
+int modulo(int num1, int num2){
+if(num1>=0)&&(num2>=1)
 {
     return num1%num2;
 }
+else{
+    printf("error");
+    return 0;
+}
+}
+
+    
