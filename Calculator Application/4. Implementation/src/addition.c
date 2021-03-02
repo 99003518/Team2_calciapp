4dbcf78fcb029e7d6b53c26e38da71042a717e77
99003518 <78554641+99003518@users.noreply.github.com>
2021-03-02 07:06:17 +0530

    Update addition.c

--- Calculator Application/4. Implementation/src/addition.c
+++ Calculator Application/4. Implementation/src/addition.c
@@ -7,3 +7,12 @@ int add(int a, int b)
     printf("Result = %d ",sum);  
     return sum;                   //Returning sum of a and b
 }
+int addOvf(int* result, int a, int b)  
+{  
+    *result = a + b;  
+    if(a > 0 && b > 0 && *result < 0)  
+        return -1;  
+    if(a < 0 && b < 0 && *result > 0)  
+        return -1;  
+     
+}  
