30ed342790dbbaae0dedc3102beb9b67c8912a48
SNEHA ANAND <78555859+99003525@users.noreply.github.com>
2021-03-09 20:41:28 +0530

    Update modulo.c

--- Calculator Application/4. Implementation/src/modulo.c
+++ Calculator Application/4. Implementation/src/modulo.c
@@ -5,7 +5,7 @@ if((num1>=0)&&(num2>=1))
     return num1%num2;        //prints the modulo of two numbers ( numerator greater than or equal to zero and denominator greater than 1)
 }
 else{
-    printf("error");       //error message for  : modulo for negative numbers , denominator equal to or less than zero 
+    printf("error");       //error message for  : modulo for negative numbers , floating point numbers or denominator equal to or less than zero 
     return 0;
 }
 }
