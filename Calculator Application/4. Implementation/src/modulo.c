6d21e3b31da56bf1d3b030332ea6d76cac8c0df2
SNEHA ANAND <78555859+99003525@users.noreply.github.com>
2021-03-09 20:40:50 +0530

    Update modulo.c

--- Calculator Application/4. Implementation/src/modulo.c
+++ Calculator Application/4. Implementation/src/modulo.c
@@ -2,10 +2,10 @@
 int modulo(int num1, int num2){        //function declaration
 if((num1>=0)&&(num2>=1))
 {
-    return num1%num2;        //prints the modulo of two numbers
+    return num1%num2;        //prints the modulo of two numbers ( numerator greater than or equal to zero and denominator greater than 1)
 }
 else{
-    printf("error");
+    printf("error");       //error message for  : modulo for negative numbers , denominator equal to or less than zero 
     return 0;
 }
 }
